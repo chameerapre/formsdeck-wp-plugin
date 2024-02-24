314b4d570ae40e356692641443cafaa399a4b223
Chameera Premasiri <chameerapremasiri@gmail.com>
2024-02-24 21:12:50 +0530

    Added readme file

---
+++ tags/0.0.1/formsdeck.php
@@ -0,0 +1,181 @@
+<?php
+
+/**
+ * Plugin Name: FormsDeck
+ * Version: 0.0.1
+ * Plugin URI: https://formsdeck.com/
+ * Description: Get form responses from the customer's WhatsApp number to yours.
+ * Author: Origyn.Company
+ * Author URI: https://origyn.company/
+ * License: GPLv2 or later
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+
+// Define plugin directory URL
+define('FORMSDECK_DIR_URL', plugin_dir_url(__FILE__));
+
+// Enqueue admin styles
+function formsdeck_enqueue_admin_styles() {
+    wp_enqueue_style('formsdeck-admin-style', plugins_url('CSS/admin.css', __FILE__), array(), time());
+}
+add_action('admin_enqueue_scripts', 'formsdeck_enqueue_admin_styles');
+
+// Add settings page to the admin menu
+function formsdeck_add_menu() {
+    $menu_icon = plugins_url('assets/formsdeck menu icon.svg', __FILE__);
+    add_menu_page(
+        'FormsDeck Settings',
+        'FormsDeck',
+        'manage_options',
+        'formsdeck-settings',
+        'formsdeck_settings_page',
+        $menu_icon
+    );
+}
+add_action('admin_menu', 'formsdeck_add_menu');
+
+// Settings page content
+function formsdeck_settings_page() {
+?>
+    <div class="wrap fd-settings-page">
+        <div class="fd-header">
+            <a href="<?php echo esc_url('https://formsdeck.com'); ?>"><img class="fd-logo" src="<?php echo esc_url(FORMSDECK_DIR_URL . 'formsdeck-logo.png'); ?>" alt="<?php echo esc_attr('FormsDeck Logo'); ?>"></a>
+        </div>
+        <?php
+        // Display admin notification if settings were successfully saved
+        if (isset($_GET['settings-updated']) && $_GET['settings-updated']) {
+        ?>
+            <div class="notice notice-success is-dismissible">
+                <p><?php esc_html_e('Settings have been successfully saved!', 'formsdeck'); ?></p>
+            </div>
+        <?php
+        }
+        ?>
+        <div class="fd-content">
+            <div class="left-panel">
+                <div class="fd-create-form">Don't have a FormsDeck form yet? <a href='https://formsdeck.com/?utm_source=wordpress' target='_blank' rel='noreferrer'> Create a new one</a></div>
+                <form method="post" action="options.php">
+                    <?php
+                    settings_fields('formsdeck_settings');
+                    do_settings_sections('formsdeck-settings');
+                    submit_button();
+                    ?>
+                </form>
+            </div>
+            <div class="right-panel">
+                <div class="fd-feedback">
+                    <h3>Show us some love :)</h3>
+                    <p>Found FormsDeck useful? Rate it and leave a nice little comment at wordpress.org. We would appreciate that.</p>
+                    <button class="fd-btn">Rate us</button>
+                </div>
+
+                <div class="fd-follow">
+                    <h3>Let's be friends 🤝</h3>
+                    <button class="fd-btn">Subscribe on YouTube</button>
+                    <a href="https://twitter.com/FormsDeckHQ"><button class="fd-btn">Follow us on X</button></a>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+<?php
+}
+
+
+/**
+ * Display instructional notice on top of dashboard pages.
+ * Removed when user dismisses the notice.
+ */
+function formsdeck_getting_started_notice() {
+    global $current_user;
+    $user_id = $current_user->ID;
+
+    if (!get_user_meta($user_id, 'formsdeck_getting_started_notice') && !(isset($_GET['page']) && $_GET['page'] == 'formsdeck')) {
+
+        $output = '<div class="notice" style="display: flex;flex-direction:column;gap:10px;padding:20px;">';
+        $output .= '<a href="' . esc_url('https://formsdeck.com/?utm_source=wordpress') . '" class="logo"><img src="' . esc_url(FORMSDECK_DIR_URL . "formsdeck-logo.png") . '" width="150px" alt="' . esc_attr__('formsdeck logo') . '"/></a>';
+        $output .= '<div><h4 style="margin: 0;">' . esc_html__('Getting started with your FormsDeck form 🚀') . '</h4><ol>';
+        $output .= '<li>' . wp_kses(sprintf(
+            __('If you are not an existing FormsDeck user, <a href="%1$s" target="_blank" rel="noreferrer">click here to register.</a>'),
+            esc_url('https://formsdeck.com/?utm_source=wordpress')
+        ), array('a' => array('href' => array(), 'target' => array(), 'rel' => array()))) . '</li>';
+        $output .= '<li>' . esc_html__('Design and publish your form') . '</li>';
+        $output .= '<li>' . wp_kses(sprintf(
+            __('Copy the widget snippet and visit <a href="%1$s">plugin settings</a> to add FormsDeck to your website.'),
+            esc_url(admin_url('admin.php?page=formsdeck-settings'))
+        ), array('a' => array('href' => array()))) . '</li>';
+        $output .= '</ol></div>';
+        $output .= '<a href="' . esc_url(admin_url('admin.php?page=formsdeck-settings')) . '&formsdeck_notice_ignore=1">' . esc_html__('Dismiss') . '</a>';
+        $output .= '</div>';
+
+        echo wp_kses_post($output);
+    }
+}
+
+
+function formsdeck_notice_ignore() {
+    global $current_user;
+    $user_id = $current_user->ID;
+    /* If user clicks to ignore the notice, add that to their user meta */
+    if (isset($_GET['formsdeck_notice_ignore']) && '1' == $_GET['formsdeck_notice_ignore']) {
+        add_user_meta($user_id, 'formsdeck_getting_started_notice', 'true', true);
+    }
+}
+add_action('admin_init', 'formsdeck_notice_ignore');
+
+
+// Register settings and fields
+function formsdeck_register_settings() {
+    register_setting('formsdeck_settings', 'formsdeck_code');
+    register_setting('formsdeck_settings', 'formsdeck_location');
+
+    add_settings_section('formsdeck_section', 'Add Widget', 'formsdeck_section_callback', 'formsdeck-settings');
+
+    add_settings_field('formsdeck_code', 'Widget Snippet', 'formsdeck_code_callback', 'formsdeck-settings', 'formsdeck_section');
+    add_settings_field('formsdeck_location', 'Show on', 'formsdeck_location_callback', 'formsdeck-settings', 'formsdeck_section');
+}
+add_action('admin_init', 'formsdeck_register_settings');
+
+// Section callback
+function formsdeck_section_callback() {
+    echo 'Insert your Formsdeck form widget snippet and choose where to display.';
+}
+
+// Code field callback
+function formsdeck_code_callback() {
+    $code = get_option('formsdeck_code');
+    echo '<textarea name="formsdeck_code" rows="5" cols="50">' . esc_textarea($code) . '</textarea>';
+}
+
+// Location field callback
+function formsdeck_location_callback() {
+    $location = get_option('formsdeck_location');
+    $options = array(
+        'everywhere' => 'Everywhere',
+        'homepage' => 'Homepage Only',
+        'except_homepage' => 'Everywhere Except Homepage',
+        'nowhere' => 'Nowhere'
+    );
+
+    echo '<select name="formsdeck_location">';
+    foreach ($options as $key => $value) {
+        echo '<option value="' . esc_attr($key) . '" ' . selected($location, $key, false) . '>' . esc_html($value) . '</option>';
+    }
+    echo '</select>';
+}
+
+// Hook to insert code snippet in the site body
+function formsdeck_insert_code() {
+    $code = get_option('formsdeck_code');
+    $location = get_option('formsdeck_location');
+
+    if ($location === 'everywhere' || ($location === 'homepage' && is_front_page()) || ($location === 'except_homepage' && !is_front_page())) {
+        echo wp_kses($code, array('script' => array('async' => array(), 'src' => array(), 'id' => array(), 'data-id' => array(), 'data-message' => array())));
+    }
+}
+add_action('wp_footer', 'formsdeck_insert_code');
---
+++ tags/0.0.1/readme.txt
@@ -0,0 +1,58 @@
+=== FormsDeck ===
+Contributors: Origyn
+Tags: whatsapp, whatsapp chat, whatsapp business, whatsapp support, whatsapp button, whatsapp form, whatsapp wordpress, whatsapp widget
+Requires at least: 4.5.0
+Requires PHP: 5.5
+Tested up to: 6.4
+Stable tag: 0.0.1
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Add a beautiful WhatsApp form widget & receive responses from customers on "WhatsApp" and "WhatsApp Business".
+
+== Description ==
+
+**💬 FormsDeck offers the ultimate integration between WhatsApp and WordPress. This is the official WordPress plugin of <a target="_blank" href="https://formsdeck.com/?utm_source=wordpress_description">FormsDeck</a>.**
+
+## What can you do with FormsDeck?
+
+- Build a beautiful form using our drag and drop builder
+- Add the form as a floating widget on your WordPress website
+- Embed the form anywhere on a WordPress page
+- Add the form as a new WordPress full page
+
+## Who should use FormDeck?
+
+- If your customer conversations happen on WhatsApp, then FormsDeck is the perfect solution for you!
+- If you are looking for a floating chat widget to redirect users to WhatsApp from your WordPress website.
+- If you want to embed a smart web form in your Contact Us page and send all the collected data to your WhatsApp business number
+- If you want as much data about the customer before you begin to chat with them on WhatsApp.
+ 
+== Frequently Asked Questions ==
+ 
+= Can I use FormsDeck for free? =
+Yes, we have a forever free plan. However, you can choose a paid plan if you need powerful features. <a href="https://formsdeck.com/pricing/?utm_source=wordpress_description">See our pricing</a>.
+
+= What is FormsDeck in brief? =
+FormsDeck is the official WordPress plugin of FormsDeck form builder. Our product allows you to create simple forms that can deliver the response from the customer to your WhatsApp number. The plugin allows you to add the form as a floating widget or as a Full page or embed the form inside a page or post in your WordPress website.
+
+= How much Coding is involved? =
+No coding involved! The form is created using our drag and drop builder, and to add the form to the website, you copy-paste the relevant code snippet to the plugin.
+
+= Can everyone see my WhatsApp number? =
+Not right away. When the user has filled the form and clicks on the Submit button, we direct the user to the WhatsApp number you have provided. There it becomes public. 
+
+= Can I send the response to a WhatsApp group chat? =
+Sorry, WhatsApp does not support sending to WhatsApp groups.
+
+= Is this GDPR compatible? =
+We do not collect user data from your website and do not store cookies.
+
+== Screenshots ==
+1. Get responses from Customer's WhatsApp Number.
+2. Easy to use Drag and Drop Builder.
+3. Plugin settings page
+
+== Changelog ==
+= 0.0.1 =
+* Plugin released.
